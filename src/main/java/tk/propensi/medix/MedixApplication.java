5b1b0681beb9045db476e4279ed7806d24f3760b
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2022-03-01 21:06:55 +0700

    initial push

---
+++ src/main/java/tk/propensi/medix/MedixApplication.java
@@ -0,0 +1,13 @@
+package tk.propensi.medix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/propensi/medix/MedixApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.propensi.medix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
