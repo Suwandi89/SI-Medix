889b358eb3f71d4b3821842c200c47c979300863
NaufalNabiel <muhammad.naufal99@ui.ac.id>
2022-05-08 13:12:42 +0700

    Memperbaiki Error

--- src/main/java/tk/propensi/medix/MedixApplication.java
+++ src/main/java/tk/propensi/medix/MedixApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, LiquibaseAutoConfiguration.class })
-public class MedixApplication extends SpringBootServletInitializer {
-
+@SpringBootApplication
+public class MedixApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedixApplication.class, args);
 	}
