bcf63f480afa6332d9ee8a766d626186db252f2e
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2022-05-18 20:54:17 +0700

    Add jenis konsultasi pie chart

--- src/main/java/tk/propensi/medix/service/KunjunganDetilService.java
+++ src/main/java/tk/propensi/medix/service/KunjunganDetilService.java
@@ -1,6 +1,9 @@
 package tk.propensi.medix.service;
 
+import java.util.HashMap;
+
 public interface KunjunganDetilService {
     int getJumlahBPJS(Long idrs);
     int getJumlahnonBPJS(Long idrs);
+    HashMap<String, Integer> getJenisKonsultasiHashmap(Long idrs);
 }
